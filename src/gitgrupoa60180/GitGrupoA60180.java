79eed6e9ef1a16f2d11c366706474a35f9dcdfad
RORLe <RORLe@DESKTOP-QL5DQK0>
2021-02-23 20:52:05 -0600

    Ejercicio X imprimir nombre

---
+++ src/gitgrupoa60180/GitGrupoA60180.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitgrupoa60180;
+
+/**
+ *
+ * @author RORLe
+ */
+public class GitGrupoA60180 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String nombre= "VICTOR BRANDON";
+        
+        System.out.println(" Mi nombre es: " + nombre );
+    }
+    
+}
